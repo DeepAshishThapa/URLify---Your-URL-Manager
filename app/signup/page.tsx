16ce87dac5bac54e5a177993c605a8103baef78e
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-12-26 19:43:16 +1030

    rendered signup component function in the page

---
+++ app/signup/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Signup from '@/components/Signup'
+
+function page() {
+    return (
+        <div className='w-screen h-[90vh] flex justify-center items-center'>
+            <Signup/>
+            
+
+        </div>
+    )
+}
+
+export default page
