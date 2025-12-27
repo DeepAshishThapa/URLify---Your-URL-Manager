7798591991cac1eab6d7e0946caa0199f6633ba9
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-12-27 19:53:47 +1030

    added global session restore on app load

---
+++ app/auth-init.tsx
@@ -0,0 +1,23 @@
+// app/auth-init.tsx
+"use client"
+
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import authservice from "@/Appwrite/AuthService/Api"
+import { login, logout } from "@/Appwrite/AuthService/authSlice"
+
+export default function AuthInit({ children }: { children: React.ReactNode }) {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authservice
+      .getAccount()
+      .then((user) => {
+        if (user) dispatch(login(user))
+        else dispatch(logout())
+      })
+      .catch(() => dispatch(logout()))
+  }, [dispatch])
+
+  return <>{children}</>
+}
