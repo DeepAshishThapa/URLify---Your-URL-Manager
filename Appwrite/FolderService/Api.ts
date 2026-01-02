696e9c899043bd95dcb2e3b9f984d3d5250b5b54
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-01-02 18:56:11 +1030

    added Apperite folder service with CRUD operations

--- Appwrite/FolderService/Api.ts
+++ Appwrite/FolderService/Api.ts
@@ -1,45 +1,47 @@
-import { Client, TablesDB, Permission, Role,ID, Query } from "appwrite";
+import { Client, TablesDB, Permission, Role, ID, Query } from "appwrite";
 import config from "@/config/config";
 import authservice from "../AuthService/Api";
 
-type createFolderInput={
-    name:string,
-    isSystem:boolean,
-    
+type createFolderInput = {
+    name: string,
+    isSystem: boolean,
+
 }
 
-type updateFolderInput={
-    rowId:string,
-    name?:string
+type updateFolderInput = {
+    rowId: string,
+    name?: string
 }
 
-class PostService{
+
+class PostService {
     private client = new Client();
     tablesDB;
-    constructor(){
+    constructor() {
         this.client
             .setEndpoint(config.appwriteURL)
             .setProject(config.appwriteProjectId)
 
-        this.tablesDB=new TablesDB(this.client)
+        this.tablesDB = new TablesDB(this.client)
 
 
     }
 
-    async createFolder({name,isSystem}:createFolderInput){
-        const userData=await authservice.getAccount()
-        const userid=userData.$id
+    async createFolder({ name, isSystem }: createFolderInput) {
+        const userData = await authservice.getAccount()
+        const userid = userData.$id
 
         return await this.tablesDB.createRow({
-            databaseId:config.appwriteDatabaseId,
-            tableId:config.appwriteFoldertableId,
-            rowId:ID.unique(),
-            data:{
+            databaseId: config.appwriteDatabaseId,
+            tableId: config.appwriteFoldertableId,
+            rowId: ID.unique(),
+            data: {
                 name,
                 isSystem,
                 userid
             },
-            permissions:[
+            permissions: [
+                 
                 Permission.read(Role.user(userid)),
                 Permission.update(Role.user(userid)),
                 Permission.delete(Role.user(userid))
@@ -50,41 +52,57 @@ class PostService{
 
     }
 
-    async listFolders(userid:string){
+    async listFolders(userid: string) {
         return await this.tablesDB.listRows({
-            databaseId:config.appwriteDatabaseId,
-            tableId:config.appwriteFoldertableId,
-            queries:[
-                Query.equal("userid",userid)
+            databaseId: config.appwriteDatabaseId,
+            tableId: config.appwriteFoldertableId,
+            queries: [
+                Query.equal("userid", userid),
+
             ]
         })
     }
 
-    async updateFolder({rowId,name}:updateFolderInput){
+    async ensureUnsavedFolder(userid: string) {
+        return await this.tablesDB.listRows({
+            databaseId: config.appwriteDatabaseId,
+            tableId: config.appwriteFoldertableId,
+            queries: [
+                Query.equal("userid", userid),
+                Query.equal("isSystem", true),
+                Query.equal("name", "Unsaved"),
+                Query.limit(1),
+
+            ]
+
+        })
+
+    }
+
+    async updateFolder({ rowId, name }: updateFolderInput) {
         return await this.tablesDB.updateRow({
-            databaseId:config.appwriteDatabaseId,
-            tableId:config.appwriteFoldertableId,
+            databaseId: config.appwriteDatabaseId,
+            tableId: config.appwriteFoldertableId,
             rowId,
-            data:{
+            data: {
                 name
             }
 
         })
 
-
     }
 
-    async deleteFolder(rowId:string){
+    async deleteFolder(rowId: string) {
         return await this.tablesDB.deleteRow({
-            databaseId:config.appwriteDatabaseId,
-            tableId:config.appwriteFoldertableId,
+            databaseId: config.appwriteDatabaseId,
+            tableId: config.appwriteFoldertableId,
             rowId
         })
 
     }
 
-    
-    
+
+
 }
-const postservice=new PostService()
-export default postservice
+const folderservice = new PostService()
+export default folderservice
