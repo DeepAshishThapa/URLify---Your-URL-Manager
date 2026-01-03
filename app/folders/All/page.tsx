0525832dcffbbc8236388d7140a1dfc55253783b
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-01-03 16:15:14 +1030

    static page for all section

---
+++ app/folders/All/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function page() {
+  return (
+    <>
+     <div className='text-lg font-semibold'>
+        All folders
+      
+    </div>
+    <div>
+      hi
+    </div>
+    </>
+   
+  )
+}
+
+export default page
