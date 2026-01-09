87ea2ddb223ecf27661848096169dcd172ed6a0d
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-01-09 15:18:55 +1030

    fetched all the links saved in the folder in the page

--- app/folders/[folderId]/page.tsx
+++ app/folders/[folderId]/page.tsx
@@ -1,23 +1,97 @@
-import React from 'react'
+"use client"
 
-type PageProps = {
-    params: {
-        folderId: string;
-    };
-};
+import { useEffect, useState } from "react"
+import { useParams } from "next/navigation"
+import folderservice from "@/Appwrite/FolderService/Api"
+import postservice from "@/Appwrite/PostService/Api"
+import authservice from "@/Appwrite/AuthService/Api"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
-async function page({ params }: PageProps) {
-    const { folderId } = await params;
-    
+type linkinput={
+  readonly $id:string,
+  url:string,
+  description?:string,
+  folderid:string,
+  userid:string
 
-    return (
-        <div>
-            <div>
-                
-            </div>
-
-        </div>
-    )
 }
 
-export default page
+function Page() {
+  const params = useParams()
+  const folderId = params?.folderId as string | undefined
+
+  const [folder, setFolder] = useState<any>(null)
+  const [loading, setLoading] = useState(true)
+  const [links, setlinks] = useState<any>(null)
+  const [userid, setuserid] = useState<null | string>(null)
+
+  useEffect(() => {
+    if (!folderId) return
+
+    folderservice.getFolder(folderId).then((res) => {
+      setFolder(res)
+      setLoading(false)
+    })
+  }, [folderId])
+
+  useEffect(() => {
+    authservice.getAccount().then((res) => {
+      setuserid(res.$id)
+    })
+      .catch((error) => {
+        console.log(error)
+      })
+
+  }, [])
+
+  useEffect(() => {
+    if (!userid || !folderId) return
+
+    postservice.listfolderlinks(userid,folderId).then((res) => {
+      setlinks(res.rows)
+    })
+
+  }, [userid, folderId])
+
+  if (loading) {
+    return <p>Loading...</p>
+  }
+
+  return (
+    <div>
+
+      <p className="font-semibold text-2xl"> {folder?.name}</p>
+      <div className="mt-10 grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-4">
+        {links &&
+          links.map((link:linkinput) => (
+            <Card className="w-full max-w-md h-60 " key={link.$id}>
+              <CardHeader>
+                <CardTitle className="h-">{link.url}</CardTitle>
+                <CardDescription></CardDescription>
+                <CardAction></CardAction>
+              </CardHeader>
+              <CardContent>
+                <p className="text-gray-400 font-semibold"> {link.description}</p>
+              </CardContent>
+              <CardFooter>
+                <p></p>
+              </CardFooter>
+            </Card>
+
+          ))
+
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Page
