62721d36d49fd8981e2f9a27509585dff1a226fe
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-01-10 14:41:00 +1030

    removed the layout design

--- app/folders/layout.tsx
+++ app/folders/layout.tsx
@@ -5,7 +5,7 @@ function layout({ children }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
-        <div className=" px-5 md:px-20 py-2 space-y-6  ">
+        <div className="  ">
             {children}
             
                 
