89c8a0732e943c954fe1df15d0c81f3d706c0e3d
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-03-21 04:44:03 +1030

    feat(auth): implement verification email sending with Resend

---
+++ helpers/sendVerificationEmail.ts
@@ -0,0 +1,22 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "@/emails/VerificationEmail";
+
+export async function sendVerificationEmail(
+    email:string,
+    username:string,
+    verifyCode:string,
+){
+    try{
+        await resend.emails.send({
+            from:'dev@hiteshchoudhary.com',
+             to: email,
+            subject: 'Mystery Message Verification Code',
+            react: VerificationEmail({ username, otp: verifyCode }),
+        })
+        return {success:true, message:'Verification email '}
+    }
+    catch(emailError){
+        console.error('Error sending verification email:', emailError);
+    return { success: false, message: 'Failed to send verification email.' };
+    }
+}
